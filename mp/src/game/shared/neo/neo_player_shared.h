9617bbdf782b83940dbfe6f2db56a5aebfa05335
Rain <Rainyan@users.noreply.github.com>
2020-01-24 01:00:46 +0200

    Set cloak AUX exhaustion rates for recon and assault

--- mp/src/game/shared/neo/neo_player_shared.h
+++ mp/src/game/shared/neo/neo_player_shared.h
@@ -40,7 +40,7 @@
 #define NEO_SUPPORT_CROUCH_SPEED (NEO_BASE_CROUCH_SPEED * NEO_SUPPORT_SPEED_MODIFIER)
 
 #define SUPER_JMP_COST 45.0f
-#define CLOAK_AUX_COST 20.0f
+#define CLOAK_AUX_COST ((GetClass() == NEO_CLASS_RECON) ? 17.5f : 19.0f)
 
 // Original NT allows chaining superjumps up ramps,
 // so leaving this zeroed for enabling movement tricks.
