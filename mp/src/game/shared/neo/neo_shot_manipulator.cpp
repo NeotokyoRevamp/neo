815d47e48ef6071f72ced68b096486f2ac82d200
Rainyan <Rainyan@users.noreply.github.com>
2020-08-27 06:51:12 +0300

    Decouple recoil and spread

--- mp/src/game/shared/neo/neo_shot_manipulator.cpp
+++ mp/src/game/shared/neo/neo_shot_manipulator.cpp
@@ -2,13 +2,12 @@
 #include "neo_shot_manipulator.h"
 #include "weapon_neobasecombatweapon.h"
 
-// NEO TODO (Rain): add weapon specific capbullets, first with debug cvars as below, and later in the weapon scripts.
 ConVar sv_neo_recoil_capbullets("sv_neo_recoil_capbullets", "10", FCVAR_CHEAT | FCVAR_REPLICATED, "Generic fallback cvar for at how many bullets should weapons reach max recoil.", true, 0.0f, false, 0.0f);
 ConVar sv_neo_recoil_capscale("sv_neo_recoil_capscale", "1", FCVAR_CHEAT | FCVAR_REPLICATED, "Generic fallback scaler for max recoil cap.", true, 0.0f, false, 0.0f);
 ConVar sv_neo_recoil_viewfollow_scale("sv_neo_recoil_viewfollow_scale", "1.0", FCVAR_CHEAT | FCVAR_REPLICATED, "Scaler for how much should player eyeangle follow the current recoil.", true, 0.0f, false, 0.0f);
 
 // NEO HACK/FIXME (Rain): Doing some temporary recoil cvars here for testing.
-// Need to clean this up later once we have good values!!
+// Need to clean these up later once we have good values!!
 #define TEMP_WEP_STR(name) #name
 #define MAKE_TEMP_WEP_RECOIL_SCALER(weapon) ConVar sv_neo_##weapon##_recoil_scale(TEMP_WEP_STR(sv_neo_##weapon##_recoil_scale), "1.0", FCVAR_REPLICATED, TEMP_WEP_STR(Temporary recoil scaler for #weapon), true, 0.01, true, 10.0)
 #define MAKE_TEMP_WEP_RECOIL_CAPBULLETS(weapon) ConVar sv_neo_##weapon##_recoil_capbullets(TEMP_WEP_STR(sv_neo_##weapon##_recoil_capbullets), "1.0", FCVAR_REPLICATED, TEMP_WEP_STR(At how many bullets should #weapon reach max recoil.), true, 0.01, true, 10.0)
--- mp/src/game/shared/neo/neo_shot_manipulator.h
+++ mp/src/game/shared/neo/neo_shot_manipulator.h
@@ -25,15 +25,28 @@ public:
 		m_pWeapon = neoWep; // we're ok with a nullptr here (ie. not an NT gun); just handle as a non-recoiled weapon then.
 
 		m_iNumBullet = numBullet;
+
+		m_vecRecoilDirection = vec3_invalid;
 	}
 
 	const Vector& ApplySpread(const Vector& vecSpread, float bias = 1.0);
+	const Vector& ApplyRecoil(const Vector& vecSpread, float bias = 1.0);
+	
+	const Vector& GetRecoilDirection() const { return m_vecRecoilDirection; }
+
+	void GetSpreadAndRecoilDirections(Vector& outSpreadDir, Vector& outRecoilDir) const
+	{
+		outSpreadDir = GetShotDirection();
+		outRecoilDir = GetRecoilDirection();
+	}
 
 private:
 	float GetVerticalRecoil() const;
 
 private:
 	int m_iNumBullet;
+
+	Vector m_vecRecoilDirection;
 
 	CNEO_Player* m_pPlayer;
 
@@ -46,6 +59,12 @@ extern ConVar sv_neo_recoil_viewfollow_scale;
 
 inline const Vector &CNEOShotManipulator::ApplySpread(const Vector& vecSpread, float bias)
 {
+	VectorAdd(ApplyRecoil(vecSpread, bias), CShotManipulator::ApplySpread(vecSpread, bias), m_vecShotDirection);
+	return GetShotDirection();
+}
+
+inline const Vector& CNEOShotManipulator::ApplyRecoil(const Vector& vecSpread, float bias)
+{
 	QAngle myangles;
 	VectorAngles(m_vecShotDirection, myangles);
 
@@ -57,10 +76,7 @@ inline const Vector &CNEOShotManipulator::ApplySpread(const Vector& vecSpread, f
 	VMatrix vm = attachedToWorld;
 	MatrixBuildRotationAboutAxis(vm, m_vecRight, GetVerticalRecoil());
 
-	m_vecShotDirection = vm.ApplyRotation(m_vecShotDirection);
-
-	CShotManipulator::ApplySpread(vecSpread, bias);
-
-	return GetResult();
+	m_vecRecoilDirection = vm.ApplyRotation(m_vecShotDirection) * bias;
+	return m_vecRecoilDirection;
 }
 #endif
